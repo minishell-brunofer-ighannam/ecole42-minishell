2e145e8e1e72db25710f24fbf7027be0909cf44c
Bruno Fernandes Valero <brunofvn6@gmail.com>
2025-11-05 01:18:11 -0300

    documenting lexer

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: valero <valero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/04 12:57:32 by brunofer          #+#    #+#             */
-/*   Updated: 2025/11/05 00:15:37 by valero           ###   ########.fr       */
+/*   Created: 2025/11/05 00:14:45 by valero            #+#    #+#             */
+/*   Updated: 2025/11/05 00:15:13 by valero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stdlib.h>
-# include "libft.h"
-# include "../src/signals.h"
-# include "../src/lexer/lexer.h"
-
-# define PROMPT "minishell$> "
+#ifndef LEXER_H
+# define LEXER_H
 
 #endif
