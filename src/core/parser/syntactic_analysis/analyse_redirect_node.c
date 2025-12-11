af51ae7f6bd9562d97f5515af7de246059a4e8eb
Bruno Fernandes Valero Nunes <brunofer@c1r4p3.42sp.org.br>
2025-12-11 14:16:03 -0300

    add syntaxe error

--- src/core/parser/syntactic_analysis/analyse_redirect_node.c
+++ src/core/parser/syntactic_analysis/analyse_redirect_node.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   analyse_redirect_node.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valero <valero@student.42.fr>              +#+  +:+       +#+        */
+/*   By: brunofer <brunofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 13:19:40 by valero            #+#    #+#             */
-/*   Updated: 2025/12/01 16:33:09 by valero           ###   ########.fr       */
+/*   Updated: 2025/12/11 12:50:46 by brunofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "syntactic_analysis_internal.h"
 
-static void	ft_analyse_adjacent_tokens(t_token *right_token);
+static void	ft_analyse_adjacent_tokens(
+				t_token *right_token, t_binary_tree_node *left_node);
 
 void	ft_analyse_redirect_node(t_binary_tree_node	*node)
 {
@@ -28,15 +29,24 @@ void	ft_analyse_redirect_node(t_binary_tree_node	*node)
 		content->tokens[0]->sintaxe_error = true;
 		return ;
 	}
-	ft_analyse_adjacent_tokens(content->tokens[1]);
+	ft_analyse_adjacent_tokens(content->tokens[1], node->left);
 }
 
-static void	ft_analyse_adjacent_tokens(t_token *right_token)
+static void	ft_analyse_adjacent_tokens(
+				t_token *right_token, t_binary_tree_node *left_node)
 {
+	t_token	*left_token;
+
 	if (!right_token)
 		return ;
+	left_token = NULL;
+	if (left_node)
+		left_token = ((t_ast_node *)left_node->content)->tokens[0];
 	if (ft_is_reserved_token(right_token)
 		|| right_token->type == TOKEN_PAREN_CLOSE
 		|| right_token->type == TOKEN_PAREN_OPEN)
 		right_token->sintaxe_error = true;
+	if (left_token && left_token->type == TOKEN_PAREN_OPEN
+		&& left_token->position > right_token->position)
+		left_token->sintaxe_error = true;
 }
